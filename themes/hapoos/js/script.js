ffe8477ab78c393ff4d5316116488a8a9f2a5923
Sumeet Pareek <sumeet.pareek@innoraft.com>
2013-03-22 18:24:41 +0530

    Home page images content without titled page

--- themes/hapoos/js/script.js
+++ themes/hapoos/js/script.js
@@ -12,8 +12,30 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
 
+$(document).ready(function(){
+	// Settings for the homepage banner slider
+	var aap = {
+		bannerSlider : function(options_){
+			// Set starting slide to 1
+			var startSlide = 1;
+			
+			// Initialize Slides
+			$(options_.selector).slides({
+				preload: true,
+				generatePagination: options_.pagination,
+				play: 5000,
+				pause: 2500,
+				hoverPause: true,
+				// Get the starting slide
+				start: startSlide
+			});
+		}
+	};
 
-// Place your code here.
+	// Initiate the homepage banner slider
+	aap.bannerSlider({selector : '#homepage-slides', pagination : false});	
+});
+
 
 
 })(jQuery, Drupal, this, this.document);
