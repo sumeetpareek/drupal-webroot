04d8ea956df826e3db3815bd68052139fbdd52e0
Kuldeep Singh Pal <tkuldeep.singh@innoraft.com>
2013-08-16 18:40:44 +0530

    slide_survey.js file is modified
    reference -> code4aap/drupal-aap-campaign#117
    
    aap_survey_tracker cookie is added

--- sites/all/themes/aap_theme/js/slide_survey.js
+++ sites/all/themes/aap_theme/js/slide_survey.js
@@ -6,17 +6,30 @@
 					// After a set amount of time (we will not make it configurable now) the specific
 					// survery pane is moved from footer to first child of the body and opened with slide effect
 
+					aap_survey_tracker = $.cookie("aap_survey_tracker");
+					survey = $('.surveymonkey-drupal-popup').detach();
+					survey_id = $(survey).attr('id');
+					
 					// TODO: Do all of this based on cookie aap_survey_tracker
 					// Hint1: Refer this for working with cookie - https://github.com/code4aap/drupal-webroot/issues/18
 					// Hint2: Use CSS ID of the pane for cookie value
-					setTimeout(function(){
-						// TODO: A 'Close' button needs to be added as last element in .surverymonkey-drupal-popup
-						// which should close the survery and set the cookie value to not show it again
-						$('body').scrollTop(0);
-						survey = $('.surveymonkey-drupal-popup').detach();
-						$('body').children().first().before(survey);
-						survey.slideDown('slow');
-					}, 6000);
+					if (!(aap_survey_tracker == survey_id)){
+						setTimeout(function(){
+							// TODO: A 'Close' button needs to be added as last element in .surverymonkey-drupal-popup
+							// which should close the survery and set the cookie value to not show it again
+							$('body').scrollTop(0);
+							survey_id = $(survey).attr('id');
+							$(survey) // Replace this selector with one suitable for you
+							.append('<input type="button" value="Close">') // Create the element
+							.click(function(){
+								$.cookie("aap_survey_tracker", survey_id);		// set cookie value that user has closed this survey
+								$('body').children().first().hide();
+							});
+							
+							$('body').children().first().before(survey);
+							survey.slideDown('slow');
+						}, 6000);
+					}
 				}
 			}
 	}	
